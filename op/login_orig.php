8e8b263934030296d82c429ed004e483ba5c653d
jackqlean <jackqlean@gmail.com>
2017-06-14 10:08:48 -0300

    Ultimas atualizações.

---
+++ op/login_orig.php
@@ -0,0 +1,112 @@
+<?php
+// inclui o arquivo de inicialização
+require_once"../config/init.php";
+require "../config/functions.php"; 
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+
+<script src="../lib/jquery/jquery-1.12.4.js"></script>
+<link href="../lib/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<script src="../lib/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link href="../lib/bootstrap-dialog/css/bootstrap-dialog.min.css" rel="stylesheet" type="text/css" />
+<script src="../lib/bootstrap-dialog/js/bootstrap-dialog.min.js"></script>
+</head>
+<body>
+  
+<?php
+// resgata variáveis do formulário
+$email = isset($_POST['txt_login']) ? $_POST['txt_login'] : '';
+$password = isset($_POST['txt_password']) ? $_POST['txt_password'] : '';
+ 
+if (empty($email) || empty($password))
+{
+    echo"<script>
+        $(document).ready(function () {
+        BootstrapDialog.show({
+            title: 'Informação do sistema',
+            message: 'Informe email e senha. Pressione Enter para continuar...',
+            onshow: function(dialog) {
+                dialog.getButton('button-ok').enable();
+            },
+            buttons: [{
+                id: 'button-ok',
+                label: 'Ok',
+                hotkey: 13,
+                cssClass: 'btn-primary',
+                action: function(){
+                     window.location.href='../index.php'
+                }
+            }]
+        });
+       });
+</script>";
+    exit;
+}
+ 
+// cria o hash da senha
+$passwordHash = make_hash($password);
+ 
+$PDO = db_connect();
+ 
+$sql = "SELECT id, name, s.setor AS setor FROM users, setor s WHERE users.cod_setor = s.cod_setor AND email = :email AND password = :password";
+$stmt = $PDO->prepare($sql);
+ 
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':password', $passwordHash);
+ 
+$stmt->execute();
+ 
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($users) <= 0)
+
+{
+   echo"<script>
+        $(document).ready(function () {
+        BootstrapDialog.show({
+            title: 'Informação do sistema',
+            message: 'Usuário ou senha inválido. Pressione Enter para continuar...',
+            onshow: function(dialog) {
+                dialog.getButton('button-ok').enable();
+            },
+            buttons: [{
+                id: 'button-ok',
+                label: 'Ok',
+                hotkey: 13,
+                cssClass: 'btn-primary',
+                action: function(){
+                     window.location.href='../index.php'
+                }
+            }]
+        });
+       });
+</script>";
+    exit;
+}
+ 
+// pega o primeiro usuário
+$user = $users[0];
+ 
+session_start();
+$_SESSION['logged_in'] = true;
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['user_name'] = $user['name'];
+$_SESSION['user_setor'] = $user['setor'];
+
+header('Location: ../index.php');
+?>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->  
+   </body>
+</html>
